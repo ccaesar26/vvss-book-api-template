cff733b9b71eb5896bc2a34b973f924b1d1849dd
Cezar Constăndoiu <93541398+ccaesar26@users.noreply.github.com>
2025-09-02 17:47:13 +0300

    Initial Commit

---
+++ src/test/java/ro/unitbv/fmi/vvss/bookapi/BookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.unitbv.fmi.vvss.bookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
